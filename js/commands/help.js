c4c76cb3ecf321434b9a021a5462b704d2e555d3
Tony Lin <ttony.lin@mail.utoronto.ca>
2018-07-07 18:22:31 -0400

    reformatted help command list

--- js/commands/help.js
+++ js/commands/help.js
@@ -4,16 +4,17 @@ module.exports = (message) => {
 
   // In format of [[command name, command description], ...]
   var array_of_commands = [
-    ['//avatar @name', 'Shows the user\'s avatar image url.'],
-    ['//emoji :custom emoji:', 'Shows the custom emoji\'s image url.'],
-    ['//help', 'Shows the list of commands with its description.'],
-    ['//server icon', 'Shows the server\'s icon url.']
+    ['//avatar @name\t\t\t', 'Shows the user\'s avatar image url.'],
+    ['//emoji :custom emoji:\t', 'Shows the custom emoji\'s image url.'],
+    ['//help\t\t\t\t\t', 'Shows the list of commands with its description.'],
+    ['//server icon \t\t\t', 'Shows the server\'s icon url.']
   ];
 
   // formatted string holding the array_of_commands to reply
   let result = 'List of commands:\n';
   for (let i = 0; i < array_of_commands.length; i++) {
-    result += array_of_commands[i][0] + ' : ' + array_of_commands[i][1] + '\n';
+    result += array_of_commands[i][0];
+    result += array_of_commands[i][1] + '\n';
   }
 
   message.reply('_Beldum Beldum_ :question:\n' + '\`' + result + '\`')
