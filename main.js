852c84063b2593c672c7a49b6e2e912b2dd68614
Tony Lin <ttony.lin@mail.utoronto.ca>
2018-07-09 17:39:43 -0400

    added server count

--- main.js
+++ main.js
@@ -2,10 +2,22 @@
 // Acquire token from .env
 require('dotenv').config()
 const token = process.env.TOKEN;
+const token_discordbot = process.env.TOKEN_DISCORDBOT;
 
 // Init discord
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const DBL = require("dblapi.js");
+const dbl = new DBL(token_discordbot, client);
+
+// Posting server count for discordbots
+dbl.on('posted', () => {
+  console.log('Server count posted!');
+})
+dbl.on('error', e => {
+ console.log(e);
+})
+
 client.login(token);
 
 // Ready check
