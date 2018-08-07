57879c68c6d88d1d257ff1baf24fcd7cdf2a7c54
Tony Lin <ttony.lin@mail.utoronto.ca>
2018-08-07 16:40:20 -0400

    tested flip command

--- js/commands/flip.js
+++ js/commands/flip.js
@@ -3,6 +3,13 @@
 module.exports = (Discord, message, prefix) => {
 
   // [Heads, Tails, Edge]
+  const results = [
+    'Heads',
+    'Tails',
+    'What the...',
+  ];
+
+  // [Heads, Tails, Edge]
   const result_images = [
     'https://github.com/Tony120914/Beldum-Bot/blob/master/images/heads.png?raw=true',
     'https://github.com/Tony120914/Beldum-Bot/blob/master/images/tails.png?raw=true',
@@ -21,15 +28,15 @@ module.exports = (Discord, message, prefix) => {
   let result;
   let image;
   if (random > 6001) {
-    result = 'Heads'; // 5999/12000 chance of heads
+    result = results[0]; // 5999/12000 chance of heads
     image = result_images[0];
   }
   else if (random > 2) {
-    result = 'Tails'; // 5999/12000 chance of tails
+    result = results[1]; // 5999/12000 chance of tails
     image = result_images[1];
   }
   else {
-    result = 'What the...'; // 1/6000 chance of edge
+    result = results[2]; // 1/6000 chance of edge
     image = result_images[2];
     rich_embed.setFooter('(Fun fact: There\'s a 1/6000 chance of a coin landing on its edge!)');
   }
@@ -40,7 +47,8 @@ module.exports = (Discord, message, prefix) => {
   ;
 
   message.channel.send(rich_embed)
-  .then(console.log('Successful coin flip'))
+  .then(console.log(`Successful command reply to ${message.content}`))
   .catch(console.error);
+  return rich_embed;
 
 }
---
+++ tests/flip_test.js
@@ -0,0 +1,47 @@
+
+// test flip command
+
+const assert = require('chai').assert;
+const Discord = require('discord.js');
+const flip = require('../js/commands/flip.js');
+
+//util
+const util = require('./util.js');
+let prefix = util.prefix;
+let message = util.mock_message();
+
+describe('flip: valid command', () => {
+  it('should return RichEmbed with certain properties', () => {
+
+    message.content = prefix + "flip";
+
+    // [Heads, Tails, Edge]
+    const results = [
+      'Heads',
+      'Tails',
+      'What the...',
+    ];
+
+    // [Heads, Tails, Edge]
+    const result_images = [
+      'https://github.com/Tony120914/Beldum-Bot/blob/master/images/heads.png?raw=true',
+      'https://github.com/Tony120914/Beldum-Bot/blob/master/images/tails.png?raw=true',
+      'https://github.com/Tony120914/Beldum-Bot/blob/master/images/edge.jpg?raw=true'
+    ];
+
+    // multi tests because of rng
+    for (let i = 0; i < (results.length * result_images.length); i++) {
+      let actual = flip(Discord, message, prefix);
+
+      // instance of RichEmbed
+      let expect = Discord.RichEmbed;
+      assert.instanceOf(actual, expect);
+      // description is within results
+      assert.isTrue(results.includes(actual.author.name));
+      // image is within result_images
+      assert.isTrue(result_images.includes(actual.image.url));
+      // result description matches result images
+      assert.equal(results.indexOf(actual.author.name), result_images.indexOf(actual.image.url));
+    }
+  })
+})
