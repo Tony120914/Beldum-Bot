972b24f1eba21006c2fb7c46578550e922c577f2
Tony Lin <ttony.lin@mail.utoronto.ca>
2024-02-01 03:37:50 -0500

    init cloudflare worker serverless, optimized file structure and file detection

---
+++ src/server.js
@@ -0,0 +1,92 @@
+/**
+ * The core server that runs on a Cloudflare worker.
+ */
+
+import { Router } from 'itty-router';
+import {
+    InteractionResponseType,
+    InteractionType,
+    verifyKey,
+} from 'discord-interactions';
+import { Commands } from './commands.js';
+
+export class JsonResponse extends Response {
+    constructor(body, init) {
+        const jsonBody = JSON.stringify(body);
+        init = init || {
+            headers: {
+                'content-type': 'application/json;charset=UTF-8',
+            },
+        };
+        super(jsonBody, init);
+    }
+}
+
+const router = Router();
+
+/**
+ * A simple :wave: hello page to verify the worker is working.
+ */
+router.get('/', (request, env) => {
+    return new Response(`👋 ${env.DISCORD_APPLICATION_ID}`);
+});
+
+/**
+ * Main route for all requests sent from Discord. All incoming messages will
+ * include a JSON payload described here:
+ * https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-object
+ */
+router.post('/', async (request, env) => {
+    const { isValid, interaction } = await server.verifyDiscordRequest(
+        request,
+        env,
+    );
+    if (!isValid || !interaction) {
+        return new Response('Bad request signature.', { status: 401 });
+    }
+
+    if (interaction.type === InteractionType.PING) {
+        // The `PING` message is used during the initial webhook handshake, and is
+        // required to configure the webhook in the developer portal.
+        return new JsonResponse({
+            type: InteractionResponseType.PONG,
+        });
+    }
+
+    if (interaction.type === InteractionType.APPLICATION_COMMAND) {
+        let commandName = interaction.data.name.toLowerCase();
+        if (Commands.has(commandName)) {
+            return Commands.get(commandName).execute(env);
+        } else {
+            return new JsonResponse({ error: 'Unknown Type' }, { status: 400 });
+        }
+    }
+
+    console.error('Unknown Type');
+    return new JsonResponse({ error: 'Unknown Type' }, { status: 400 });
+});
+router.all('*', () => new Response('Not Found.', { status: 404 }));
+
+async function verifyDiscordRequest(request, env) {
+    const signature = request.headers.get('x-signature-ed25519');
+    const timestamp = request.headers.get('x-signature-timestamp');
+    const body = await request.text();
+    const isValidRequest =
+        signature &&
+        timestamp &&
+        verifyKey(body, signature, timestamp, env.DISCORD_PUBLIC_KEY);
+    if (!isValidRequest) {
+        return { isValid: false };
+    }
+
+    return { interaction: JSON.parse(body), isValid: true };
+}
+
+const server = {
+    verifyDiscordRequest: verifyDiscordRequest,
+    fetch: async function (request, env) {
+        return router.handle(request, env);
+    },
+};
+
+export default server;
