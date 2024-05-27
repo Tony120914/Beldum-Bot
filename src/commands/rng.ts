e382448fecb5493d1cfb2cca5f0f1d3b6fbfb0c4
Tony Lin <ttony.lin@mail.utoronto.ca>
2024-05-27 15:47:58 -0400

    rng command now has button for rerolling

--- src/commands/rng.ts
+++ src/commands/rng.ts
@@ -1,9 +1,10 @@
 import { ApplicationCommand, ApplicationCommandOption } from '../templates/discord/ApplicationCommand.js'
 import { Command } from '../templates/app/Command.js';
-import { APPLICATION_COMMAND_OPTION_TYPE, APPLICATION_COMMAND_TYPE, INTERACTION_RESPONSE_TYPE } from '../templates/discord/Enums.js';
+import { APPLICATION_COMMAND_OPTION_TYPE, APPLICATION_COMMAND_TYPE, BUTTON_STYLE, INTERACTION_RESPONSE_TYPE, INTERACTION_TYPE } from '../templates/discord/Enums.js';
 import { Embed } from '../templates/discord/Embed.js';
 import { InteractionResponse } from '../templates/discord/InteractionResponse.js'
 import { getRandomInt } from '../handlers/Utils.js';
+import { ButtonNonLink, ActionRow } from '../templates/discord/MessageComponents.js';
 
 const applicationCommand = new ApplicationCommand(
     'rng',
@@ -28,17 +29,37 @@ SecondNumberInputOption.setRequired(true);
 applicationCommand.addOptions(SecondNumberInputOption);
 
 const execute = async function(interaction: any, env: any, args: string[]) {
-    const int1 = interaction.data.options[0].value; // using raw value instead of args to avoid converting
-    const int2 = interaction.data.options[1].value; // using raw value instead of args to avoid converting
-    const randomInt = getRandomInt(Math.min(int1, int2), Math.max(int1, int2));
+    const interactionResponse = new InteractionResponse(INTERACTION_RESPONSE_TYPE.CHANNEL_MESSAGE_WITH_SOURCE);
+    let int1: number = 0;
+    let int2: number = 0;
+    if (interaction.type == INTERACTION_TYPE.APPLICATION_COMMAND) {
+        // Using raw values instead of args to avoid converting
+        int1 = interaction.data.options[0].value;
+        int2 = interaction.data.options[1].value;
+    }
+    else if (interaction.type == INTERACTION_TYPE.MESSAGE_COMPONENT) {
+        interactionResponse.setType(INTERACTION_RESPONSE_TYPE.UPDATE_MESSAGE);
+        const data = JSON.parse(interaction.data.custom_id);
+        int1 = data.int1;
+        int2 = data.int2;
+    }
+    const min = Math.min(int1, int2);
+    const max = Math.max(int1, int2);
+    const randomInt = getRandomInt(min, max);
 
     const embed = new Embed();
     embed.setTitle('Random Number Generator');
-    embed.setDescription(`Between ${int1} and ${int2} (both inclusive)`);
+    embed.setDescription(`Between ${min} and ${max} (both inclusive)`);
     embed.addField('Result', randomInt.toString());
-    
-    const interactionResponse = new InteractionResponse(INTERACTION_RESPONSE_TYPE.CHANNEL_MESSAGE_WITH_SOURCE);
     interactionResponse.data?.addEmbed(embed);
+
+    const data = {"int1": int1, "int2": int2};
+    const button = new ButtonNonLink(JSON.stringify(data)); // Passing data through custom_id
+    button.setStyle(BUTTON_STYLE.PRIMARY);
+    button.setLabel('Roll again');
+    const actionRow = new ActionRow();
+    actionRow.addComponent(button);
+    interactionResponse.data?.addComponent(actionRow);
 
     return interactionResponse;
 }
